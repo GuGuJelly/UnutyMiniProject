6ad6be13b3596d920e7001ca41ffb6c90c4f1567
GuGuJelly <jellypuding@naver.com>
2024-09-27 17:42:14 +0900

    NavMesh 플레이어 추적과 복귀 구현
    NavMesh 플레이어 추적과 복귀 구현

---
+++ Assets/MonsterScripts/FireMonster.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireMonster : MonoBehaviour
+{
+    [SerializeField] float monsterBulletSpeed;
+
+    private void Update()
+    {
+        
+    }
+
+}
--- Assets/MonsterScripts/NavMover.cs
+++ Assets/MonsterScripts/NavMover.cs
@@ -8,7 +8,7 @@ public class NavMover : MonoBehaviour
 {
     [SerializeField] NavMeshAgent agent;
     [SerializeField] Transform target;
-    [SerializeField] BoxCollider boxCollider;
+    [SerializeField] Transform returnPoint;
 
     private void Awake()
     {
@@ -16,23 +16,57 @@ public class NavMover : MonoBehaviour
         agent.isStopped = true;
     }
 
-    private void Update()
-    {
-        NavChaserMonster();
-    }
 
-    private void NavChaserMonster()
+    //private void NavChaserMonster()
+    //{
+    //    if (boxCollider.CompareTag("Player"))
+    //    {
+    //        transform.LookAt(target);
+    //        agent.isStopped = false;
+    //        agent.destination = target.position;
+    //
+    //        if (boxCollider == null)
+    //        {
+    //            agent.isStopped = true;
+    //        }
+    //    }
+    //}
+
+    private void OnTriggerEnter(Collider other)
     {
-        if (boxCollider.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
-            // transform.LookAt(target);
+            transform.LookAt(target);
+            agent.stoppingDistance = 8;
             agent.isStopped = false;
             agent.destination = target.position;
-
-            if (boxCollider == null)
-            {
-                agent.isStopped = true;
-            }
         }
     }
+
+    private void OnTriggerStay(Collider other)
+    {
+        // 탱크의 이동속도가 너무 빨라서 가속이 못따라가면 
+        // 탱크가 목적지점까지 이동했을때 지나쳐가버려서
+        // 갈팡질팡하기 때문에 가속을 충분히 높게 해주면 해결된다
+        if (other.gameObject.CompareTag("Player")) 
+        {
+            transform.LookAt(target);
+            agent.stoppingDistance = 8;
+            agent.isStopped = false;
+            agent.destination = target.position;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            transform.LookAt(returnPoint);
+            agent.stoppingDistance = 0;
+            agent.destination = returnPoint.position;
+        }
+        agent.isStopped = false;
+    }
+
+    
 }
