ec0f5222926e5d598025d839b80eb2e7f4a06aaa
GuGuJelly <jellypuding@naver.com>
2024-10-02 18:13:02 +0900

    게임 과제 제출버전
    게임 과제 제출버전

---
+++ Assets/Scripts/VictoryScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryScript : MonoBehaviour
+{
+    [SerializeField] GameObject monsterTankPrefab;
+    [SerializeField] float monsterTankCount;
+
+    private void MonsterTankDestroy()
+    {
+
+    }
+}
